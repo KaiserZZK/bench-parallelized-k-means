ce61bb0dca19bf9f8e19e65297b4f11c4dcb57cb
jiangxian0613 <85988263+jiangxian0613@users.noreply.github.com>
2023-04-30 21:03:12 -0400

    Update cukmeans_alexminnaar.cu

--- cukmeans_alexminnaar.cu
+++ cukmeans_alexminnaar.cu
@@ -1,26 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cfloat>
+#include <chrono>
+#include <fstream>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
-#define N 64
 #define TPB 1024
+#define MAX_K 25 // number of clusters cannot go above this number
 
 __device__ float distance(float x1, float x2, float y1, float y2) {
 	return pow((y1-x1),2) + pow((y2-x2),2);
 }
 
-__global__ void kMeansClusterAssignment(float *d_datapoints, int *d_clust_assn, float *d_centroids, int *d_k, int *d_n) {
+__global__ void kMeansClusterAssignment(float *d_datapoints, int *d_clust_assn, float *d_centroids, int K, int N) {
 	//get idx for this datapoint
 	const int idx = blockIdx.x*blockDim.x + threadIdx.x;
 
 	//bounds check
-	if (idx >= d_n) return;
+	if (idx >= N) return;
 
 	//find the closest centroid to this datapoint
 	float min_dist = INFINITY;
 	int closest_centroid = 0;
 
-	for(int c = 0; c<d_k;++c) { // note, now variables like k and n must be passed into the function to work
+	for(int c = 0; c<K;++c) { // note, now variables like k and n must be passed into the function to work
 		float dist = distance(d_datapoints[2*idx], d_datapoints[2*idx+1], d_centroids[2*c], d_centroids[2*c+1]);
 
 		if(dist < min_dist) {
@@ -34,31 +45,37 @@ __global__ void kMeansClusterAssignment(float *d_datapoints, int *d_clust_assn, 
 }
 
 
-__global__ void kMeansCentroidUpdate(float *d_datapoints, int *d_clust_assn, float *d_centroids, int *d_clust_sizes, int *d_k, int *d_n) {
+__global__ void kMeansCentroidUpdate(float *d_datapoints, int *d_clust_assn, float *d_centroids, int *d_clust_sizes, const int K, const int N) {
 	//get idx of thread at grid level
 	const int idx = blockIdx.x*blockDim.x + threadIdx.x;
 
 	//bounds check
-	if (idx >= d_n) return;
+	if (idx >= N) return;
 
 	//get idx of thread at the block level
 	const int s_idx = threadIdx.x;
 
 	//put the datapoints and corresponding cluster assignments in shared memory so that they can be summed by thread 0 later
 	__shared__ float s_datapoints[2*TPB];
+    
 	s_datapoints[2*s_idx]= d_datapoints[2*idx];
     s_datapoints[2*s_idx+1]= d_datapoints[2*idx+1];
 
 	__shared__ int s_clust_assn[TPB];
 	s_clust_assn[s_idx] = d_clust_assn[idx];
 
+
+	s_datapoints[2*s_idx]= d_datapoints[2*idx];
+    s_datapoints[2*s_idx+1]= d_datapoints[2*idx+1];
+
+
 	__syncthreads();
 
 	// it is the thread with idx 0 (in each block) that sums up all the values within the shared array for the block it is in
     // COMMENT: this part could maybe be optimized? (could end up being 1 thread doing most of the computation)
 	if(s_idx==0) {
-		float b_clust_datapoint_sums[2*d_k]={0};
-		int b_clust_sizes[d_k]={0};
+		float b_clust_datapoint_sums[2*MAX_K]={0};
+		int b_clust_sizes[MAX_K]={0};
 
 		for(int j=0; j< blockDim.x; ++j) {
 			int clust_id = s_clust_assn[j];
@@ -68,7 +85,7 @@ __global__ void kMeansCentroidUpdate(float *d_datapoints, int *d_clust_assn, flo
 		}
 
 		//Now we add the sums to the global centroids and add the counts to the global counts.
-		for(int z=0; z < d_k; ++z) {
+		for(int z=0; z < K; ++z) {
 			atomicAdd(&d_centroids[2*z],b_clust_datapoint_sums[2*z]);
             atomicAdd(&d_centroids[2*z+1],b_clust_datapoint_sums[2*z+1]);
 			atomicAdd(&d_clust_sizes[z],b_clust_sizes[z]);
@@ -78,14 +95,14 @@ __global__ void kMeansCentroidUpdate(float *d_datapoints, int *d_clust_assn, flo
 	__syncthreads();
 
 	//currently centroids are just sums, so divide by size to get actual centroids
-	if(idx < d_k){
+	if(idx < K){
 		d_centroids[idx] = d_centroids[idx]/d_clust_sizes[idx]; 
 	}
 
 }
 
 
-int main()
+int main(int argc, char* argv[] )
 {
     if (argc < 5) {
         std::cerr << "usage: k-means <data-file> <k> <n> [iterations]" << std::endl; // where n is the number of datapoints of the form (x1, x2)
@@ -124,17 +141,11 @@ int main()
 	float *d_centroids = 0;
 	//allocate memory on the device for the cluster sizes
 	int *d_clust_sizes=0;
-    //allocate memory on the device for number of clusters and number of data
-    int *d_k=0; int *d_n=0;
 
 	cudaMalloc(&d_datapoints, 2*n*sizeof(float));
 	cudaMalloc(&d_clust_assn, n*sizeof(int));
 	cudaMalloc(&d_centroids, 2*k*sizeof(float));
 	cudaMalloc(&d_clust_sizes, k*sizeof(float));
-    cudaMalloc(&d_k, sizeof(int));
-	cudaMalloc(&d_n, sizeof(int));
-    cudaMemcpy(d_k, &k, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_n, &n, sizeof(int), cudaMemcpyHostToDevice);
 
 	srand(time(0));
 
@@ -153,18 +164,21 @@ int main()
 	int cur_iter = 0;
 
     const auto start = std::chrono::high_resolution_clock::now();
-
-	while(cur_iter < number_of_iterations)
-	{
+   
+    
+    const int threads = TPB;
+    const int blocks = (n + threads - 1) / threads;
+	
+    while(cur_iter < number_of_iterations) {
 		//call cluster assignment kernel
-		kMeansClusterAssignment<<<(n+TPB-1)/TPB,TPB>>>(d_datapoints,d_clust_assn,d_centroids,d_k,d_n);
+		kMeansClusterAssignment<<<blocks,threads>>>(d_datapoints,d_clust_assn,d_centroids,k,n);
 
 		//reset centroids and cluster sizes (will be updated in the next kernel)
 		cudaMemset(d_centroids,0.0,2*k*sizeof(float));
-		cudaMemset(d_clust_sizes,0,2*K*sizeof(int));
+		cudaMemset(d_clust_sizes,0,2*k*sizeof(int));
 
 		//call centroid update kernel
-		kMeansCentroidUpdate<<<(N+TPB-1)/TPB,TPB>>>(d_datapoints,d_clust_assn,d_centroids,d_clust_sizes,d_k,d_n);
+		kMeansCentroidUpdate<<<blocks,threads>>>(d_datapoints,d_clust_assn,d_centroids,d_clust_sizes,k,n);
 
 		cur_iter+=1;
 	}
@@ -183,24 +197,17 @@ int main()
     //code for timing
     const auto end = std::chrono::high_resolution_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
-    std::cerr << "Standard CUDA implementation Took: " << duration.count() << "s" << " for "<<h_x.size()<<" points."<<std::endl;
-    std_time_used = duration.count();
+    std::cerr << "Standard CUDA implementation Took: " << duration.count() << "s" << " for "<< n <<" points."<<std::endl;
+    float std_time_used = duration.count();
     
-    cudaFree(d_counts);
-
-    std::vector<float> mean_x(k, 0);
-    std::vector<float> mean_y(k, 0);
-    cudaMemcpy(mean_x.data(), d_means.x, d_means.bytes, cudaMemcpyDeviceToHost);
-    cudaMemcpy(mean_y.data(), d_means.y, d_means.bytes, cudaMemcpyDeviceToHost);
 
     FILE *fp;
-    int i;
 
     fp = fopen("Standardtimes.txt", "a");
         fprintf(fp, "%0.6f\n", std_time_used);
     fclose(fp);
 
-    std::string str(std::to_string(h_x.size())),str1,str2;
+    std::string str(std::to_string(n)),str1,str2;
     str = "output/standard/" + str;
     
     str2 = str + "_centroids.txt";
